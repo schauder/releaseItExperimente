a699e3e1860e2233b029a1b6841c6b722be048d0
René Pfeuffer <rene@pfeuffer-web.de>
2015-01-05 13:48:17 +0100

    minor details

--- src/main/java/de/schauderhaft/releaseit/evilserver/controller/EvilController.java
+++ src/main/java/de/schauderhaft/releaseit/evilserver/controller/EvilController.java
@@ -1,9 +1,11 @@
 package de.schauderhaft.releaseit.evilserver.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class EvilController {
 
     @RequestMapping("/ok")
