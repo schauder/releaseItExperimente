4fb1318134782638d11a2c04a448d9ddd209aa9b
René Pfeuffer <rene@pfeuffer-web.de>
2015-01-05 12:59:28 +0100

    simple call to bane
    on port 8090

--- src/main/java/de/schauderhaft/releaseit/main/controller/SampleController.java
+++ src/main/java/de/schauderhaft/releaseit/main/controller/SampleController.java
@@ -1,5 +1,7 @@
 package de.schauderhaft.releaseit.main.controller;
 
+import de.schauderhaft.releaseit.main.service.BaneService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 
+    private BaneService baneService;
+
+    @Autowired
+    public SampleController(BaneService baneService) {
+        this.baneService = baneService;
+    }
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
+        baneService.connectToBane();
         return "Hello World!";
     }
 }
--- src/main/java/de/schauderhaft/releaseit/main/service/BaneService.java
+++ src/main/java/de/schauderhaft/releaseit/main/service/BaneService.java
@@ -1,8 +1,14 @@
 package de.schauderhaft.releaseit.main.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class BaneService {
 
+    public void connectToBane() {
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject("http://localhost:8090", String.class);
+        System.out.println(response);
+    }
 }
