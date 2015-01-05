295db8af6aa19e76b87cb423f1eae160abab044b
René Pfeuffer <rene@pfeuffer-web.de>
2015-01-05 13:41:44 +0100

    evil server
    port 8000

---
+++ src/main/java/de/schauderhaft/releaseit/evilserver/EvilServerStarter.java
@@ -0,0 +1,15 @@
+package de.schauderhaft.releaseit.evilserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"de.schauderhaft.releaseit"})
+public class EvilServerStarter {
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("server.port", "8000");
+        SpringApplication.run(EvilServerStarter.class, args);
+    }
+}
---
+++ src/main/java/de/schauderhaft/releaseit/evilserver/controller/EvilController.java
@@ -0,0 +1,30 @@
+package de.schauderhaft.releaseit.evilserver.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class EvilController {
+
+    @RequestMapping("/ok")
+    @ResponseBody
+    String home() {
+        return "Hello, World!";
+    }
+
+    @RequestMapping("/wait/{time}")
+    @ResponseBody
+    String wait(@PathVariable("time") Long time) {
+        try {
+            Thread.sleep(time * 1000);
+        } catch (InterruptedException e) {
+        }
+        return "Hello, World!";
+    }
+
+    @RequestMapping("/500")
+    @ResponseBody
+    String gimmefive() {
+        throw new RuntimeException("Good bye, World!");
+    }
+}
