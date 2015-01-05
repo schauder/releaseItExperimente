6d6f0c8b90db2c16815f3d248bfed501f55caf34
René Pfeuffer <rene@pfeuffer-web.de>
2015-01-05 13:34:42 +0100

    added diffenet behaviours

--- src/main/java/de/schauderhaft/releaseit/main/controller/SampleController.java
+++ src/main/java/de/schauderhaft/releaseit/main/controller/SampleController.java
@@ -3,6 +3,7 @@ package de.schauderhaft.releaseit.main.controller;
 import de.schauderhaft.releaseit.main.service.BaneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,10 +17,25 @@ public class SampleController {
         this.baneService = baneService;
     }
 
-    @RequestMapping("/")
+    @RequestMapping("/ok")
     @ResponseBody
     String home() {
-        baneService.connectToBane();
-        return "Hello World!";
+        return "Hello, World!";
+    }
+
+    @RequestMapping("/wait/{time}")
+    @ResponseBody
+    String wait(@PathVariable("time") Long time) {
+        try {
+            Thread.sleep(time * 1000);
+        } catch (InterruptedException e) {
+        }
+        return "Hello, World!";
+    }
+
+    @RequestMapping("/500")
+    @ResponseBody
+    String gimmefive() {
+        throw new RuntimeException("Good bye, World!");
     }
 }
