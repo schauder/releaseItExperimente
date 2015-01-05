65e32f9b78af31605a5f1bdaf689229c925e47a1
Jens Schauder <jens.schauder@freenet.de>
2015-01-05 12:57:42 +0100

    git experimente

--- src/main/java/de/schauderhaft/releaseit/main/controller/SampleController.java
+++ src/main/java/de/schauderhaft/releaseit/main/controller/SampleController.java
@@ -3,7 +3,7 @@ package de.schauderhaft.releaseit.main.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+ // nur für testzwecke
 @Controller
 public class SampleController {
 
